9600554cf496e0429276093cddfc2add039a17ae
soorajkumar-2005 <soorajku2021@gmail.com>
2023-07-19 09:58:30 +0500

    sign up validation added

---
+++ signUp.js
@@ -0,0 +1,122 @@
+let fullname = document.getElementById("name");
+let fatherName = document.getElementById("fatherName");
+let emailAddress = document.getElementById("emailAddress");
+let newPassword = document.getElementById("newPassword");
+let confirmPassword = document.getElementById("confirmPassword");
+let cnicNumber = document.getElementById("cnicNumber");
+let mobileNumber = document.getElementById("mobileNumber");
+let selectCountry = document.getElementById("selectCountry");
+let gender = document.getElementById("gender");
+let qualification = document.getElementById("qualification");
+let selectCourses = document.getElementById("selectCourses");
+let profilePhoto = document.getElementById("profilePhoto");
+let message = document.getElementById("message");
+let logo = document.getElementById("logo");
+let success = document.getElementById("success");
+let successMessage = document.getElementById("successMessage");
+
+
+// sign up function
+function signUp() {
+  if (fullname.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = "Name is required";
+    
+    setTimeout(function () {
+      fullname.focus();
+    }, 2000);
+  } else if (fatherName.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = " Father Name is required";
+    setTimeout(function () {
+      fatherName.focus();
+    }, 2000);
+  } else if (emailAddress.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = "Email Address is required";
+    setTimeout(function () {
+      emailAddress.focus();
+    }, 2000);
+  } else if (newPassword.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = " Password is required";
+    setTimeout(function () {
+      newPassword.focus();
+    }, 2000);
+  } else if (confirmPassword.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = " Confirm Password is required";
+    setTimeout(function () {
+      confirmPassword.focus();
+    }, 2000);
+  } else if (confirmPassword.value != newPassword.value) {
+    logo.style.display = "inline";
+    message.innerHTML = "Password does not match";
+    setTimeout(function () {
+      newPassword.focus();
+    }, 2000);
+  } else if (cnicNumber.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = " CNIC Number is required";
+    setTimeout(function () {
+      cnicNumber.focus();
+    }, 2000);
+  } else if (mobileNumber.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = " Mobile Number is required";
+    setTimeout(function () {
+      mobileNumber.focus();
+    }, 2000);
+  } else if (selectCountry.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = " Country is required";
+    setTimeout(function () {
+      selectCountry.focus();
+    }, 2000);
+  } else if (gender.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = " Gender is required";
+    setTimeout(function () {
+      gender.focus();
+    }, 2000);
+  } else if (qualification.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = " Qualification is required";
+    setTimeout(function () {
+      qualification.focus();
+    }, 2000);
+  } else if (selectCourses.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = " Course Selection is required";
+    setTimeout(function () {
+      selectCourses.focus();
+    }, 2000);
+  } else if (profilePhoto.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = " Profile Photo is required";
+    setTimeout(function () {
+      profilePhoto.focus();
+    }, 2000);
+  } else {
+    success.style.display = "inline";
+    successMessage.style.display = "inline";
+    successMessage.innerHTML = "Successfully Registered";
+    setTimeout(function () {
+      fullname.value = "";
+      fatherName.value = "";
+      emailAddress.value = "";
+      newPassword.value = "";
+      confirmPassword.value = "";
+      cnicNumber.value = "";
+      mobileNumber.value = "";
+      selectCountry.value = "";
+      gender.value = "";
+      qualification.value = "";
+      selectCourses.value = "";
+      profilePhoto.value = "";
+    }, 5000);
+  }
+}
+function continueWithGoogle() {
+  window.location.href = "https://www.google.com";
+}
