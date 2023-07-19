cbdfc5a4c8a2d6d9adfb72ae103757b565fe39d1
soorajkumar-2005 <soorajku2021@gmail.com>
2023-07-19 13:28:45 +0500

    login validations improved

--- login/login.js
+++ login/login.js
@@ -1,10 +1,34 @@
 let email = document.getElementById("email");
 let password = document.getElementById("password");
-let message = document.getElementById("message");
 let logo = document.getElementById("logo");
+let message = document.getElementById("message");
 let success = document.getElementById("success");
 let successMessage = document.getElementById("successMessage");
 
-function login(){
-    
+function login() {
+  if (email.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = "Email Address is required";
+    setTimeout(function () {
+      email.focus();
+    }, 2000);
+  } else if (password.value == "") {
+    logo.style.display = "inline";
+    message.innerHTML = "Password is required";
+    setTimeout(function () {
+      password.focus();
+    }, 2000);
+  } else {
+    logo.style.display = "none";
+    message.style.display = "none";
+    success.style.display = "inline";
+    successMessage.style.display = "inline";
+    successMessage.innerHTML = "Logged In Successfully";
+    setTimeout(function () {
+      successMessage.style.display = "none";
+      success.style.display = "none";
+      email.value = "";
+      password.value = "";
+    }, 5000);
+  }
 }
